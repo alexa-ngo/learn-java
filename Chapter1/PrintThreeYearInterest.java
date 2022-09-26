51b15cef7ee456835e77bc20e0ea62e7d1e9fd9f
ango <alexaariango@outlook.com>
2022-09-26 16:13:39 -0700

    Chapter1: PrintThreeYearInterest.java

---
+++ Chapter1/PrintThreeYearInterest.java
@@ -0,0 +1,13 @@
+public class PrintThreeYearInterest
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Year One:");
+        System.out.println(1000*0.05);
+        System.out.println("Year Two:");
+        System.out.println(1050*0.05);
+        System.out.println("Year Three:");
+        System.out.println(1052.5*0.05);
+    }
+}
+
