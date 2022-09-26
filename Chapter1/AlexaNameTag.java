779c5aa328431e0c5e2dfa59844d890fb8deeebf
ango <alexaariango@outlook.com>
2022-09-26 16:20:30 -0700

    Chapter1: AlexaNameTag.java Revision

--- Chapter1/AlexaNameTag.java
+++ Chapter1/AlexaNameTag.java
@@ -2,8 +2,8 @@ public class AlexaNameTag
 {
     public static void main(String[] args)
     {
-        System.out.println("   -------------  ")
-        System.out.println("   |   Alexa   |  ")
-        System.out.println("   -------------  ")
+        System.out.println("   -------------  ");
+        System.out.println("   |   Alexa   |  ");
+        System.out.println("   -------------  ");
     }
 }
