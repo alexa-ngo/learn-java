f1e83aa3b22b5deca7e3079063d471efb61a5b3b
ango <alexaariango@outlook.com>
2022-09-27 14:26:07 -0700

    Lesson2: Variables

---
+++ Chapter2-Variables/SimpleClass.java
@@ -0,0 +1,21 @@
+import simplepackage.Rectangle;
+
+public class SimpleClass {	
+	public static void main(String[] args) {
+		// typeName labelName = value;
+		Rectangle rectangle1 = new Rectangle(20,30);
+		Rectangle rectangle2 = new Rectangle(900,100);
+	
+		System.out.println("Rectangle 1 width " + rectangle1.getWidth());
+		System.out.println("Rectangle 1 height " + rectangle1.getHeight());
+		System.out.println("Rectangle 1 area " + rectangle1.getArea());
+	
+		System.out.println("Rectangle 2 width " + rectangle2.getWidth());
+		System.out.println("Rectangle 2 height " + rectangle2.getHeight());
+		System.out.println("Rectangle 2 area " + rectangle2.getArea());
+	
+	
+		}
+}
+
+
