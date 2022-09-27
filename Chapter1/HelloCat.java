7e97cb29f4b12106ead6dfb3cc54ee0067f78636
ango <alexaariango@outlook.com>
2022-09-27 07:26:24 -0700

    Chapter1: HelloCat.java

---
+++ Chapter1/HelloCat.java
@@ -0,0 +1,11 @@
+public class HelloCat
+{
+    public static void main(String[] args)
+    {
+        System.out.println("    /\\_/\\        -------     ");
+        System.out.println("   { ' ' }      /  Hello  \\   ");
+        System.out.println("    ( - )      <  Friend! |   ");
+        System.out.println("    | | |       \\   <3    /    ");
+        System.out.println("   (__|__)       -------     ");
+    }
+}
