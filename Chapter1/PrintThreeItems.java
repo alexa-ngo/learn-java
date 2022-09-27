4e0c607bc829e0108c3819cfa26564e198b6235b
ango <alexaariango@outlook.com>
2022-09-27 07:43:06 -0700

    Chapter1: PrintThreeItems.java

---
+++ Chapter1/PrintThreeItems.java
@@ -0,0 +1,9 @@
+public class PrintThreeItems
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Intel: 13th Generation is coming out!");
+        System.out.println("AMD: The newly released AMD Ryzen 9 7950x!");
+        System.out.println("NVIDIA: GeForce power!");
+    }
+}
