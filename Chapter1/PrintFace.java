1921a9e9c3c3fa94c13575dd9e89bc6ef0ef71a4
ango <alexaariango@outlook.com>
2022-09-27 05:44:12 -0700

    Chapter1: PrintFace.java

---
+++ Chapter1/PrintFace.java
@@ -0,0 +1,12 @@
+public class PrintFace
+{
+    public static void main(String[] args)
+    {
+        System.out.println("     /////     ");
+        System.out.println("    +XXXXX+    ");
+        System.out.println("   (| O O |)   ");
+        System.out.println("   <|  w <|    ");
+        System.out.println("    |     |    ");
+        System.out.println("    +++++++    ");
+    }
+}
