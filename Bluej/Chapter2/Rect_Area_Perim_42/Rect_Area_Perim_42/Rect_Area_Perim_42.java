6a86def923e474da197a78c954c8bc91910cc97f
ango <alexaariango@outlook.com>
2022-09-29 08:03:36 -0700

    Chapter2-E2.5-Rect_Area_Perim_42.java

---
+++ Bluej/Chapter2/Rect_Area_Perim_42/Rect_Area_Perim_42/Rect_Area_Perim_42.java
@@ -0,0 +1,14 @@
+import java.awt.Rectangle;
+
+public class Rect_Area_Perim_42
+{
+    public static void main(String[] args)
+    {
+        Rectangle box = new Rectangle(0,0,19,2);
+        
+        // Print information about width and height
+        
+        System.out.println(box.getWidth());
+        System.out.println(box.getHeight());
+    }
+}
