b0f6c5eccd94f5c26c90599c12f8b9f719070e31
ango <alexaariango@outlook.com>
2022-09-29 11:55:31 -0700

    Chapter2-E2.1-Mississippi-Replace-Rev1-java

--- Bluej/Chapter2/MississippiReplace/MississippiReplace/MississippiReplace.java
+++ Bluej/Chapter2/MississippiReplace/MississippiReplace/MississippiReplace.java
@@ -3,7 +3,10 @@ public class MississippiReplace
     public static void main(String[] args)
     {
         String stateName = "Mississippi";
-        System.out.println(stateName.replace("i","ii"));
-        System.out.println(stateName.replace("ss","s"));
+        String replace_i_with_ii = stateName.replace("i","ii");
+        String replace_ss_with_s = replace_i_with_ii.replace("ss","s");
+        
+        System.out.println("\"Replaces i with ii\": " + replace_i_with_ii);
+        System.out.println("\"Replaces ss with s\": " + replace_ss_with_s);
     }
 }
